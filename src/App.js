c0cc79fadb8f251eccdbad3a7ed8cf385408ced3
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-16 08:34:53 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from "react";
-import "./style.css";
-
+import React from 'react';
+import './style.css';
+function Header() {
+  return (
+    <header>
+      <div>logoname</div>
+      <div>
+        <a href="">home</a>
+        <a href="">about</a>
+        <a href="">contact</a>
+        <a href="">services</a>
+      </div>
+    </header>
+  );
+}
+function About() {
+  return (
+    <div className="about">
+      <h1>About Us </h1>
+      <p>we are about us </p>
+      <button>read me </button>
+    </div>
+  );
+}
 export default function App() {
   return (
     <div>
-
-
+      <Header />
+      <About />
     </div>
   );
 }
